c910a0ac3a15ee5fabbb192a17dcc7a8d80468fa
nieldm <nieldm@gmail.com>
2018-09-07 20:00:33 -0500

    WIP

--- freeMarknad/ViewControllers/EnterAmount/EnterAmountVC.m
+++ freeMarknad/ViewControllers/EnterAmount/EnterAmountVC.m
@@ -1,38 +1,51 @@
-//
-//  EnterAmountVC.m
-//  freeMarknad
-//
-//  Created by nieldm on 9/6/18.
-//  Copyright © 2018 nieldm. All rights reserved.
-//
-
 #import "EnterAmountVC.h"
+#import <Masonry.h>
 
 @interface EnterAmountVC ()
+
+@property (nonatomic, strong) UITextField *amountTextField;
+@property (nonatomic, strong) UIButton *payButton;
 
 @end
 
 @implementation EnterAmountVC
 
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.view setBackgroundColor:[UIColor blueColor]];
-    // Do any additional setup after loading the view.
+
+    [self.navigationController setNavigationBarHidden:TRUE];
+
+    _amountTextField = [[UITextField alloc] initWithFrame:CGRectZero];
+    [_amountTextField setBackgroundColor:[UIColor yellowColor]];
+    [_amountTextField setKeyboardType:UIKeyboardTypeNumberPad];
+    [self.view addSubview:_amountTextField];
+    [_amountTextField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.view);
+        make.width.equalTo(self.view).multipliedBy(0.7);
+        make.height.mas_equalTo(45);
+    }];
+
+    _payButton = [[UIButton alloc] initWithFrame:CGRectZero];
+    [_payButton setTitle:@"Pay" forState:UIControlStateNormal];
+    [_payButton setUserInteractionEnabled:TRUE];
+    [_payButton setBackgroundColor:[UIColor yellowColor]];
+    [self.view addSubview:_payButton];
+    [_payButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_amountTextField.mas_bottom).with.offset(12);
+        make.centerX.equalTo(_amountTextField.mas_centerX);
+        make.width.equalTo(_amountTextField.mas_width);
+        make.height.mas_equalTo(45);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
