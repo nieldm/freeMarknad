308557ef98a9322fb162bd30c049ec6846bdd755
nieldm <nieldm@gmail.com>
2018-09-09 16:19:37 -0500

    Added Core Data

---
+++ freeMarknad/ViewControllers/EnterAmount/EnterAmountViewModel.h
@@ -0,0 +1,17 @@
+//
+//  EnterAmountViewModel.h
+//  freeMarknad
+//
+//  Created by Daniel Mendez on 9/9/18.
+//  Copyright © 2018 nieldm. All rights reserved.
+//
+
+#import "RVMViewModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EnterAmountViewModel : RVMViewModel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ freeMarknad/ViewControllers/MasterViewController.m
@@ -0,0 +1,32 @@
+//
+//  MasterViewController.m
+//  freeMarknad
+//
+//  Created by Daniel Mendez on 9/9/18.
+//  Copyright © 2018 nieldm. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@end
+
+@implementation MasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
