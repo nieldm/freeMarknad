5e520099d0c84f7d639789773b1ef7c457c36e44
nieldm <nieldm@gmail.com>
2018-09-09 16:20:23 -0500

    Added Master View Controller

--- freeMarknad/ViewControllers/MasterViewModel.h
+++ freeMarknad/ViewControllers/MasterViewModel.h
@@ -1,17 +1,9 @@
-//
-//  MasterViewModel.h
-//  freeMarknad
-//
-//  Created by Daniel Mendez on 9/9/18.
-//  Copyright © 2018 nieldm. All rights reserved.
-//
-
 #import "RVMViewModel.h"
 
-NS_ASSUME_NONNULL_BEGIN
+@class EnterAmountVC;
 
 @interface MasterViewModel : RVMViewModel
 
-@end
+-(EnterAmountVC *)enterAmount;
 
-NS_ASSUME_NONNULL_END
+@end
