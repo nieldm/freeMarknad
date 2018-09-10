a68ac787c40206f94c792ba8ed9dbe676a8ead2a
nieldm <nieldm@gmail.com>
2018-09-10 00:30:32 -0500

    Updated gitignore

---
+++ freeMarknad/Services/MercadoPagoAPI.h
@@ -0,0 +1,13 @@
+//
+//  MercadoPagoAPI.h
+//  freeMarknad
+//
+//  Created by Daniel Mendez on 9/9/18.
+//  Copyright © 2018 nieldm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MercadoPagoAPI : NSObject
+
+@end
---
+++ freeMarknad/Services/NetworkConstants.m
@@ -0,0 +1,13 @@
+//
+//  NetworkConstants.m
+//  freeMarknad
+//
+//  Created by Daniel Mendez on 9/10/18.
+//  Copyright © 2018 nieldm. All rights reserved.
+//
+
+#import "NetworkConstants.h"
+
+@implementation NetworkConstants
+
+@end
