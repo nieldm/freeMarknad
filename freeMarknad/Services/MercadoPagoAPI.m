8e6c7ac31123576b661e58822263a735fa7808e3
nieldm <nieldm@gmail.com>
2018-09-10 00:30:32 -0500

    Updated gitignore

---
+++ freeMarknad/Services/MercadoPagoAPI.m
@@ -0,0 +1,13 @@
+//
+//  MercadoPagoAPI.m
+//  freeMarknad
+//
+//  Created by Daniel Mendez on 9/9/18.
+//  Copyright © 2018 nieldm. All rights reserved.
+//
+
+#import "MercadoPagoAPI.h"
+
+@implementation MercadoPagoAPI
+
+@end
---
+++ freeMarknad/Services/PaymentService.m
@@ -0,0 +1,13 @@
+//
+//  PaymentService.m
+//  freeMarknad
+//
+//  Created by Daniel Mendez on 9/9/18.
+//  Copyright © 2018 nieldm. All rights reserved.
+//
+
+#import "PaymentService.h"
+
+@implementation PaymentService
+
+@end
