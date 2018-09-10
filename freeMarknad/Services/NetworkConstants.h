9d7ab8540e3cd88152d88a2a669f0417407932a0
nieldm <nieldm@gmail.com>
2018-09-10 00:30:32 -0500

    Updated gitignore

---
+++ freeMarknad/Services/NetworkConstants.h
@@ -0,0 +1,13 @@
+//
+//  NetworkConstants.h
+//  freeMarknad
+//
+//  Created by Daniel Mendez on 9/10/18.
+//  Copyright © 2018 nieldm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkConstants : NSObject
+
+@end
