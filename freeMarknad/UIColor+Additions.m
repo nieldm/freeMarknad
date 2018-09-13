67e1f6f1fc7aa46293a89f31a0296b3dcdf59b81
nieldm <nieldm@gmail.com>
2018-09-12 19:42:28 -0500

    Improved EnterAmount UI

---
+++ freeMarknad/UIColor+Additions.m
@@ -0,0 +1,29 @@
+//
+//  UIColor+Additions.m
+//  Preo_iOS
+//
+//  Generated on Zeplin. (9/12/2018).
+//  Copyright (c) 2018 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIColor+Additions.h"
+
+@implementation UIColor (Additions)
+
++ (UIColor * _Nonnull)main {
+    return [UIColor colorWithHexString:@"383B4E"];
+}
+
++ (UIColor * _Nonnull)secondary {
+    return [UIColor colorWithHexString:@"6D98F6"];
+}
+
++ (UIColor * _Nonnull)gradientTop {
+    return [UIColor colorWithHexString:@"3C3E53"];
+}
+
++ (UIColor * _Nonnull)gradientBottom {
+    return [UIColor colorWithHexString:@"5F607D"];
+}
+
+@end
